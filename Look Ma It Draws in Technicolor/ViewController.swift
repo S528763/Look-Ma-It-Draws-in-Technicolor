602e5636754537aafcfbe1de0db8a635023546de
student <s528763@ch140033m.nwmissouri.edu>
2017-11-27 17:37:03 -0600

    Initial Commit

---
+++ Look Ma It Draws in Technicolor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Look Ma It Draws in Technicolor
+//
+//  Created by Srimat Tirumala Pallerlamudi,Aditya on 11/27/17.
+//  Copyright © 2017 Aditya Srimat Tirumala Pallerlamudi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
